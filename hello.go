4520ca66d5eefd38ad3fa94d35086e5d9ca953ae
Harald Weidner <hweidner@gmx.net>
2019-11-14 19:42:38 +0100

    removed Markdown in package documentation

--- hello.go
+++ hello.go
@@ -1,7 +1,7 @@
 /*
 Command hello greets you with a friendly message.
 
-This repository is just there to test the new Go developer site [go.dev](https://go.dev/).
+This repository is just there to test the new Go developer site https://go.dev/
 */
 package main
 
